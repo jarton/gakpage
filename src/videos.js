540b7f24e2ff0ab32b427b3dab03f9aa9e7ffe46
jarton <jdrton@icloud.com>
2016-08-25 18:13:36 +0200

    fixed reponsiveness of videos, fb plugin and videos

--- src/videos.js
+++ src/videos.js
@@ -15,9 +15,15 @@ function addClickListerns() {
 			$('.selected-vid').children().show()
 			$('.selected-vid > iframe').remove()
 
+			let vidWidth = Math.floor($(window).width() * 0.8)
+			if (vidWidth >= 1200) {
+				vidWidth = 1100	
+			}
+			let vidHeight= Math.floor(vidWidth / 2)
 			//show new video
 		let html = '<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F338312802949307%2Fvideos%2Fvb.338312802949307%2F' + event.target.id + 
-		'%2F%3Ftype%3D2%26theater&width=1150&show_text=false&height=545&appId" width="1150" height="545" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>'
+		'%2F%3Ftype%3D2%26theater&width='+vidWidth+'&show_text=false&height='+vidHeight+'&appId" width="'+vidWidth+'" height="'+vidHeight+'" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>'
+			console.log(html)
 			html = $.parseHTML(html)
 			$('#'+ event.target.id).parent().children().hide()
 			$('#'+ event.target.id).parent().removeClass('col-md-4')
